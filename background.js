b51fad19768a9a4eb22f2dd42d0398085f2fe94b
ehuang47 <44561409+ehuang47@users.noreply.github.com>
2021-12-02 22:05:05 -0800

    gapi is set up and sync button works
    figured out the logic for the remaining functionality and wrote it out in TODOs

--- background.js
+++ background.js
@@ -26,15 +26,30 @@ chrome.runtime.onInstalled.addListener((details) => {
 });
 
 // listen for content script port connection
+var gapi_loaded = false;
 chrome.runtime.onConnect.addListener((port) => {
 	console.log("connected to port: \n", port);
 	if (port.name === "content") {
 		// when content script loads, bg script sends hello, creates a listener for messages from the port and when it disconnects. it logs any response from the content script
-		port.postMessage({ text: "hello" });
 		port.onMessage.addListener((msg, resPort) => {
-			console.log("content responded: \n", msg);
 			if (msg.message === "open_new_tab") {
 				chrome.tabs.create({ url: msg.url });
+			}
+
+			/* content script loads, triggers the list route handler, which creates the section and sync button
+      if user clicks sync now, it alerts the user, saying it will scan the entire inbox and take a while
+      when user clicks yes, content script sends a message to background script asking for the data
+      a listener in bg script will make gapi calls to prepare the data and then store it in chrome.storage
+      when finished, it sends a response to content script to check the storage
+      content script receives message and will refresh the page
+      */
+			if (gapi_loaded) {
+				if (msg.message === "sync") {
+					console.log("syncing now");
+					// TODO: generate list of subscribers (rowdescriptor objects) up until last sync timestamp using gapi & the current sync timestamp to store in chrome.storage
+					// TODO: set rowdescriptor title to name + emailaddress, body to timestamp + email subject, shortdetail text for unsubscribe link, and onclick to a function that'll check storage, grabs the unsub link if exists or else do nothing, opens a new tab for user to fill out, update labels to one that says "unsubbed timestamp", nullify the unsub link, and refresh that tab
+					port.postMessage({ message: "updated_subscribers" });
+				}
 			}
 		});
 		// gets rid of listeners when disconnected?
@@ -57,6 +72,7 @@ chrome.runtime.onConnect.addListener((port) => {
 
 //* Gmail API OAuth2 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // make sure to have user click a button (maybe on popup?) in order to activate interactive signin and access token, so that u can brief them why they need to sign in
+// https://gist.github.com/omarstreak/7908035c91927abfef59 --> reference code
 chrome.identity.getAuthToken({ interactive: true }, (token) => {
 	// retrieves an OAuth2 access token for making http request to API functions, then load Google's javascript client libraries... does identity auto-remove invalid cached tokens?
 
@@ -65,7 +81,9 @@ chrome.identity.getAuthToken({ interactive: true }, (token) => {
   not sure why gapi.auth is called to authorizer a new token, when chrome.identity should've done so already. i believe we can go straight to gapi client load
   */
 	window.gapi_onload = () => {
-		gapi.client.load("gmail", "v1", gmailAPILoaded);
+		gapi.client.load("gmail", "v1", () => {
+			gapi_loaded = true;
+		});
 	};
 
 	// make http request to load the API client script
@@ -78,10 +96,6 @@ chrome.identity.getAuthToken({ interactive: true }, (token) => {
 	request.open("GET", "https://apis.google.com/js/client.js");
 	request.send();
 });
-
-function gmailAPILoaded() {
-	//do stuff here
-}
 
 /* here are some utility functions for making common gmail requests */
 function getThreads(query, labels) {
--- js/content.js
+++ js/content.js
@@ -4,32 +4,28 @@
 var port = chrome.runtime.connect({ name: "content" });
 
 port.onMessage.addListener((msg, resPort) => {
-	console.log(msg);
-	if (msg.text === "hello") resPort.postMessage({ text: "hello, i am content" });
+	// when the subscribers are updated, i need to refresh the page so they appear in the section
+	if (msg.message === "updated_subscribers") window.location.reload(true);
 });
 
-// ONE-TIME MESSAGING
-// when message is sent by extension process(runtime.sendMessage) or content script(tabs.sendMessage)
+/* ONE-TIME MESSAGING
+when message is sent by extension process(runtime.sendMessage) or popup script(tabs.sendMessage)
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 	// gets the message and sender, as well as a function to respond with
 	console.log(msg);
 
 	if (msg.colour) document.body.style.backgroundColor = msg.colour;
-	if (msg.text === "open_browser") {
+	if (msg.message === "open_browser") {
 		var firstHref = $("a[href^='http']").eq(0).attr("href");
 		console.log(firstHref);
 		port.postMessage({ message: "open_new_tab", url: firstHref });
 		// return true; only when u need to send response asynchronously (after an async function)
-	}
+  }
 });
+*/
 
 InboxSDK.load(2, "sdk_gmanager_284293dc99").then(function (sdk) {
-	// sdk.Router.handleCustomRoute(CUSTOM, (CustomRouteView) => {
-	//   routeID = CustomRouteView.getRouteID();
-	// });
-
-	/*
-conversations to see inside messages
+	/* conversations to see inside messages
 toolbars (hover over individual email row to press unsub button)
 router for modifying the inbox page route
 lists for modifying/reading the lists ?
@@ -37,10 +33,21 @@ lists for modifying/reading the lists ?
 
 	// grab a list of emails? from the current inbox, display most recent 50 of unique emails, list email and sender and thread name
 	sdk.Router.handleListRoute(sdk.Router.NativeListRouteIDs.INBOX, (ListRouteView) => {
+		// TODO: check chrome.storage for sync timestamp and subscriber list. depending on presence or lack thereof, set the value for a last_synced string and sub_list array
+		// TODO: if a row descriptor's label is set, compare it to current timestamp. if older than 1 week, update the storage and then set the rows
+		// TODO: set contentElement to a nicely styled html notice/instructions on how to use the subscription
+		var last_synced = ""; // set to "Last synced: timestamp"
+		var subs_list = []; // set to the array in storage
 		var collap_section = ListRouteView.addCollapsibleSection({
 			title: "Subscriptions",
-			subtitle: "Expand to view the list of emails that you're currently subscribed to.",
-			tableRows: [],
+			subtitle:
+				"To unsubscribe from an email address, click on the respective row. It will navigate you to a new tab where you can fill out the request to unsubscribe. If you've recently unsubscribed from an email address and would like to delete all of their emails from your inbox, first copy and paste their email address into the search bar and hit enter. Check the box to select all emails, then select the option to 'Select all conversations that match this search'. Now that you've selected all of their emails, click the trashbin 'Delete' icon to delete them." +
+				last_synced,
+			titleLinkText: "Sync Now",
+			onTitleLinkClick: () => {
+				port.postMessage({ message: "sync" });
+			},
+			tableRows: subs_list,
 		});
 	});
 
@@ -58,16 +65,24 @@ lists for modifying/reading the lists ?
 	});
 
 	sdk.Lists.registerThreadRowViewHandler((ThreadRowView) => {
-		// for each visible thread, look at sender email and thread title, take most recent/unique email and append to list to show in subscriptions tab, ONLY if "unsubscribe" link is within the message
-		ThreadRowView.addLabel({
-			title: "Subscribed",
-			foregroundColor: "white",
-			backgroundColor: "gold",
-		});
+		// TODO: check the contacts (emailAddress and name) to see if they appear in the chrome.storage subscriber list. if so, then add the "Subscribed" label
+		var contact = ThreadRowView.getContacts()[0];
+		console.log(contact.emailAddress, contact.name);
+		let subscribed = false;
+		if (subscribed)
+			ThreadRowView.addLabel({
+				title: "Subscribed",
+				foregroundColor: "white",
+				backgroundColor: "gold",
+			});
+
+		/* currently, this opens the thread in a new tab
+    TODO: figure out how to use this functionality in the activation of clicking row descriptor
 		ThreadRowView.addButton({
 			title: "Unsubscribe",
 			iconUrl: "../images/gman.png",
-			onClick: (event) => {
+      onClick: (event) => {
+        // check chrome storage for all the emails
 				event.threadRowView.getThreadIDIfStableAsync().then((id) => {
 					// grab the current inbox url and re-direct to the thread id to open in new tab
 					let thread_url = $("a[href^='http']").eq(0).attr("href").slice(0, -5) + "#inbox/" + id;
@@ -75,6 +90,7 @@ lists for modifying/reading the lists ?
 				});
 			},
 		});
+    */
 	});
 
 	// getSelectedThreadRowView - for every view in this array, check for unsub link and then activate it ?
