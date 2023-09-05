5c83c108efb6b6e640e828923a6d820d5c660762
ehuang47 <44561409+ehuang47@users.noreply.github.com>
2023-09-05 14:49:18 -0400

    Adding webpack to bundle inboxsdk as an npm module

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,5 @@
 // import { getThreads } from "./scripts/EmailService";
 // import * as c from "./scripts/constants";
-// if es6 import fails, use dynamic import? https://stackoverflow.com/questions/48104433/how-to-import-es6-modules-in-content-script-for-chrome-extension
-// https://developer.chrome.com/docs/extensions/migrating/to-service-workers/#register-listeners says it should work, just make sure type:module is specified
 
 // when extension is installed, updated, or chrome is updated
 chrome.runtime.onInstalled.addListener((details) => {
@@ -23,49 +21,50 @@ chrome.runtime.onInstalled.addListener((details) => {
   });
 });
 
-// chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-//   try {
-//     const storage = await chrome.storage.local
-//       .get([c.GAPI_LOADED, c.LAST_SYNCED, c.REDUNDANT_EMAILS, c.START]);
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  // console.log(message, sender, sendResponse);
+  try {
+    const storage = await chrome.storage.local
+      .get([c.GAPI_LOADED, c.LAST_SYNCED, c.REDUNDANT_EMAILS, c.START]);
 
-//     let gapi_loaded = storage.gapi_loaded ?? false,
-//       last_synced = storage.last_synced ?? null,
-//       redundant_emails = storage.redundant_emails ?? false,
-//       start = storage.start ?? null;
+    let gapi_loaded = storage.gapi_loaded ?? false,
+      last_synced = storage.last_synced ?? null,
+      redundant_emails = storage.redundant_emails ?? false,
+      start = storage.start ?? null;
 
 
-//     console.log("service_worker chrome.runtime.onMessage.sender", sender);
-//     // todo: if it makes a difference, check sender = content.js, then wrap both if statements
-//     if (message.message === c.OPEN_NEW_TAB) {
-//       chrome.tabs.create({ url: message.url });
-//     }
+    console.log("service_worker chrome.runtime.onMessage.sender", sender);
+    // todo: if it makes a difference, check sender = content.js, then wrap both if statements
+    if (message.message === c.OPEN_NEW_TAB) {
+      chrome.tabs.create({ url: message.url });
+    }
 
-//     if (gapi_loaded) {
-//       const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    if (gapi_loaded) {
+      const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
 
-//       if (message.message === c.SYNC) {
-//         start = new Date().getTime();
-//         redundant_emails = false; // reset bool for every sync request
-//         chrome.storage.local.set({ start, redundant_emails });
+      if (message.message === c.SYNC) {
+        start = new Date().getTime();
+        redundant_emails = false; // reset bool for every sync request
+        chrome.storage.local.set({ start, redundant_emails });
 
-//         // see if we've synced before, and use the existing subscriber list & sync time
-//         // if (Object.keys(all_subs).length != 0)
-//         console.log("Sync in progress. Last synced at: ", last_synced);
-//         await getThreads();
-//         chrome.tabs.sendMessage(tab.id, { message: c.UPDATED_SUBSCRIBERS });
-//       }
+        // see if we've synced before, and use the existing subscriber list & sync time
+        // if (Object.keys(all_subs).length != 0)
+        console.log("Sync in progress. Last synced at: ", last_synced);
+        await getThreads();
+        chrome.tabs.sendMessage(tab.id, { message: c.UPDATED_SUBSCRIBERS });
+      }
 
-//       // mostly for testing, if you need to clear out subscriber list
-//       if (message.message === c.RESET) {
-//         chrome.storage.local.clear();
-//         chrome.tabs.sendMessage(tab.id, { message: c.UPDATED_SUBSCRIBERS });
-//       }
-//       // TODO: footer button onclick that deletes all the null-unsub link rows and refreshes tab
-//     }
-//   } catch (e) {
-//     console.warn("error runtime.onMessage.listener", e);
-//   }
-// });
+      // mostly for testing, if you need to clear out subscriber list
+      if (message.message === c.RESET) {
+        chrome.storage.local.clear();
+        chrome.tabs.sendMessage(tab.id, { message: c.UPDATED_SUBSCRIBERS });
+      }
+      // TODO: footer button onclick that deletes all the null-unsub link rows and refreshes tab
+    }
+  } catch (e) {
+    console.warn("error runtime.onMessage.listener", e);
+  }
+});
 
 //* Gmail API OAuth2 
 // TODO: have user click a button (maybe on popup?) in order to activate interactive signin and access token, so that u can brief them why they need to sign in
---
+++ src/content.js
@@ -0,0 +1,154 @@
+import * as InboxSDK from '@inboxsdk/core';
+
+InboxSDK.load(2, "sdk_gmanager_284293dc99").then((sdk) => {
+  // the SDK has been loaded, now do something with it!
+  sdk.Compose.registerComposeViewHandler((composeView) => {
+    // a compose view has come into existence, do something with it!
+    composeView.addButton({
+      title: "My Nifty Button!",
+      iconUrl:
+        "https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365",
+      onClick(event) {
+        event.composeView.insertTextIntoBodyAtCursor("Hello World!");
+      },
+    });
+  });
+});
+
+// (async () => {
+//   const constantsSrc = chrome.runtime.getURL("./scripts/constants.js");
+//   const c = await import(constantsSrc);
+
+//   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     console.log(sender.tab ?
+//       "from a content script:" + sender.tab.url :
+//       "from the extension");
+
+//     if (request.message === c.UPDATED_SUBSCRIBERS) window.location.reload(true);
+//     sendResponse();
+//   });
+
+//   function instructionHTML() {
+//     var parent = document.createElement("div");
+//     var notice = document.createElement("p");
+//     notice.innerHTML =
+//       "If you've recently unsubscribed from an email address and would like to delete all of their emails from your inbox, follow the steps below.";
+//     parent.appendChild(notice);
+
+//     var instructions = document.createElement("ol");
+//     parent.appendChild(instructions);
+//     var steps = [
+//       "Copy and paste their email address into the search bar and hit enter.",
+//       "Check the box to select all emails, then select the option to 'Select all conversations that match this search'.",
+//       "With all emails selected, click the trash bin 'Delete' icon. ",
+//     ];
+//     for (var i = 0; i < 3; i++) {
+//       let step = document.createElement("li");
+//       step.setAttribute("id", "li" + i);
+//       step.innerHTML = steps[i];
+//       instructions.appendChild(step);
+//     }
+//     return parent;
+//   }
+
+//   async function loadInboxSDK() {
+//     try {
+//       var res = await chrome.storage.local.get([c.ALL_SUBS, c.LAST_SYNCED]);
+//       const sdk = await InboxSDK.load(2, "sdk_gmanager_284293dc99");
+//       // grab a list of emails? from the current inbox, display most recent 50 of unique emails, list email and sender and thread name
+//       sdk.Router.handleListRoute(sdk.Router.NativeListRouteIDs.INBOX, (ListRouteView) => {
+//         var last_synced = "";
+//         var all_subs = [];
+
+//         if (Object.keys(res).length != 0) {
+//           let subs = res.all_subs;
+//           for (key in subs) {
+//             // key = email, subs = { [name, unsub link, isSubscribed bool], ... }
+//             // console.log(key, subs[key]);
+//             all_subs.push({
+//               title: subs[key][0],
+//               body: key,
+//               shortDetailText: "Unsubscribe",
+//               isRead: true,
+//               labels: subs[key][2]
+//                 ? [{ title: "Subscribed", foregroundColor: "white", backgroundColor: "gold" }]
+//                 : [{ title: "Unsubscribed", foregroundColor: "white", backgroundColor: "pink" }],
+//             });
+//           }
+
+//           all_subs.sort((a, b) => {
+//             let fa = a.body.toLowerCase(),
+//               fb = b.body.toLowerCase();
+
+//             if (fa < fb) {
+//               return -1;
+//             }
+//             if (fa > fb) {
+//               return 1;
+//             }
+//             return 0;
+//           });
+//           last_synced = "Last synced: " + new Date(res.last_synced).toString();
+//         }
+
+//         ListRouteView.addCollapsibleSection({
+//           title: "Subscriptions",
+//           subtitle:
+//             last_synced === ""
+//               ? last_synced
+//               : "You are currently subscribed to " + all_subs.length + " emails. " + last_synced,
+//           titleLinkText: "Sync Now",
+//           onTitleLinkClick: () => {
+//             // port.postMessage({ message: "sync" });
+//             chrome.runtime.sendMessage({ message: c.SYNC });
+//           },
+//           tableRows: all_subs,
+//           contentElement: instructionHTML(),
+//         });
+
+//         // scan the document for the html injected by inboxsdk and add onclick functionality
+//         let node_list = document.querySelectorAll(".inboxsdk__resultsSection_tableRow.zA.yO");
+//         for (let i = 0; i < node_list.length; i++) {
+//           var spans = node_list[i].querySelectorAll("span");
+//           // console.log(spans[0], spans[1], spans[2]); // should be sender, email, unsubscribe link
+//           let key = spans[1].innerText;
+//           spans[2].addEventListener("click", (e) => {
+//             let subs = res.all_subs;
+//             subs[key][2] = false;
+//             chrome.storage.local.set({ all_subs: subs });
+//             // port.postMessage({ message: "open_new_tab", url: subs[key][1] });
+//             chrome.runtime.sendMessage({ message: c.OPEN_NEW_TAB, url: subs[key][1] });
+//           });
+//         }
+
+//         let test = document.querySelector(".inboxsdk__resultsSection .zE");
+//         test.children[0].addEventListener("click", (e) => {
+//           console.log("clicked subscription heading, resetting");
+//           // port.postMessage({ message: c.RESET });
+//           chrome.runtime.sendMessage({ message: c.RESET });
+//         });
+//       });
+
+//       // for each thread row that we see, attach a label to indicate if this is a subscribed email
+//       sdk.Lists.registerThreadRowViewHandler((ThreadRowView) => {
+//         var contact = ThreadRowView.getContacts()[0];
+//         if (res.all_subs != null && res.all_subs[contact.emailAddress] != null)
+//           res.all_subs[contact.emailAddress][2]
+//             ? ThreadRowView.addLabel({
+//               title: "Subscribed",
+//               foregroundColor: "white",
+//               backgroundColor: "gold",
+//             })
+//             : ThreadRowView.addLabel({
+//               title: "Unsubscribed",
+//               foregroundColor: "white",
+//               backgroundColor: "pink",
+//             });
+//       });
+//     } catch (e) {
+//       console.warn("content.js error", e);
+//     }
+//   }
+
+//   loadInboxSDK();
+// })();
